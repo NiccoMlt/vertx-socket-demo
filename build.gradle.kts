102494227b258020e05dda80ad8f6612d743dd83
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-12-26 19:02:28 +0100

    build: add ktlint configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     kotlin("jvm") version "1.3.61"
     id("io.vertx.vertx-plugin") version "1.0.1"
+    id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
 }
 
 group = "org.example.vertx"
@@ -8,6 +11,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -32,4 +36,16 @@ tasks {
 vertx {
     mainVerticle = "org.example.vertx.MainVerticle"
     vertxVersion = "3.8.4"
+}
+
+ktlint {
+    version.set("0.36.0")
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.PLAIN)
+    }
 }
