d61e8a525a3cd8090cda3d9e2d123e18af986749
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-12-27 12:11:01 +0100

    perf(core-ws): convert client to CoroutineVerticle

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Vert.x Socket implementations
 
-## Vert.x WebSocket
+## Vert.x Core WebSocket
 
-
+Following [official example](https://github.com/vert-x3/vertx-examples/tree/master/core-examples/src/main/kotlin/io/vertx/example/core/http/websockets)
+in [Vert.x documentation](https://github.com/vert-x3/vertx-examples/blob/master/core-examples/README.adoc#websockets-echo-example),
+implement a simple WebSocket echo demo project with Vert.x Core.
--- src/main/kotlin/org/example/vertx/websocket/Client.kt
+++ src/main/kotlin/org/example/vertx/websocket/Client.kt
@@ -2,24 +2,24 @@ package org.example.vertx.websocket
 
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.logging.LoggerFactory
+import io.vertx.kotlin.core.http.webSocketAwait
+import io.vertx.kotlin.coroutines.CoroutineVerticle
 
-class Client : io.vertx.core.AbstractVerticle() {
+class Client : CoroutineVerticle() {
   private val logger = LoggerFactory.getLogger(this.javaClass)
 
-  override fun start() {
+  override suspend fun start() {
     val client = vertx.createHttpClient()
 
-    client.webSocket(8080, "localhost", "/some-uri") { res ->
-      if (res.succeeded()) {
-        val websocket = res.result()
-        websocket.handler { data ->
-          logger.info("Received data ${data.toString("ISO-8859-1")}")
-          client.close()
-        }
-        websocket.writeBinaryMessage(Buffer.buffer("Hello world"))
-      } else {
-        logger.error(res.cause())
+    try {
+      val websocket = client.webSocketAwait(8080, "localhost", "/some-uri")
+      websocket.handler { data ->
+        logger.info("Received data ${data.toString("ISO-8859-1")}")
+        client.close()
       }
+      websocket.writeBinaryMessage(Buffer.buffer("Hello world"))
+    } catch (ex: Exception) {
+      logger.error(ex.message)
     }
   }
 }
