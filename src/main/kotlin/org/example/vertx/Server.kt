20af9905ef8b7d252bc9f53bebae2d7799b18ea3
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-12-27 22:59:18 +0100

    feat(web-sockjs-eventbus): add basic EventBus SockJS bridge

--- src/main/kotlin/org/example/vertx/Server.kt
+++ src/main/kotlin/org/example/vertx/Server.kt
@@ -2,7 +2,10 @@ package org.example.vertx
 
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.LoggerFormat
+import io.vertx.ext.web.handler.LoggerHandler
 import io.vertx.ext.web.handler.StaticHandler
+import io.vertx.ext.web.handler.sockjs.BridgeOptions
 import io.vertx.ext.web.handler.sockjs.SockJSHandler
 import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions
 import io.vertx.kotlin.core.http.listenAwait
@@ -14,24 +17,37 @@ class Server : CoroutineVerticle() {
 
   override suspend fun start() {
     val router: Router = Router.router(vertx)
+
+    router.route().handler(LoggerHandler.create(LoggerFormat.SHORT))
+
+    // SockJS WebSocket-like server
     val sockJSOptions: SockJSHandlerOptions = sockJSHandlerOptionsOf(heartbeatInterval = 2000)
     val sockJSHandler: SockJSHandler = SockJSHandler.create(vertx, sockJSOptions)
-
     sockJSHandler.socketHandler { sockJSSocket ->
-      logger.info(sockJSSocket.headers())
+      // logger.info(sockJSSocket.headers())
       sockJSSocket.handler { buffer ->
         val stringBuffer = buffer.toString()
-        logger.info(stringBuffer)
+        // logger.info(stringBuffer)
         sockJSSocket.write(stringBuffer)
       }
     }
-
     router
       .route("/sock/*")
       .handler(sockJSHandler)
     router
       .route("/ws.html")
       .handler { it.response().sendFile("ws.html") }
+
+    // SockJS EventBus Bridge
+    val sockBridgeOptions = BridgeOptions()
+    val sockJsBridge = sockJSHandler.bridge(sockBridgeOptions)
+    router
+      .mountSubRouter("/eventbus", sockJsBridge)
+    router
+      .route("/eb.html")
+      .handler { it.response().sendFile("eb.html") }
+
+    // React.js frontend
     router
       .get()
       .handler(StaticHandler.create())
